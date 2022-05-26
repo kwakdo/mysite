b2e840326f1fea598d4db797db9f5ca6196cb67a
kwakdo <vv05044@gmail.com>
2022-05-26 11:31:27 +0900

    login

--- mysite02/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite02/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.douzone.mysite.repository;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.douzone.mysite.vo.UserVo;
@@ -15,39 +16,36 @@ public class UserRepository {
 		PreparedStatement pstmt = null;
 		try {
 			conn = getConnection();
-			
-			String sql =
-					" insert" +
-					"   into user" +
-					" values (null, ?, ?, ?, ?, now())";
+
+			String sql = " insert" + "   into user" + " values (null, ?, ?, ?, ?, now())";
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, vo.getName());
 			pstmt.setString(2, vo.getEmail());
 			pstmt.setString(3, vo.getPassword());
 			pstmt.setString(4, vo.getGender());
-			
+
 			int count = pstmt.executeUpdate();
 			result = count == 1;
-			
+
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
 			try {
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-		}		
-		
-		return result;		
+		}
+
+		return result;
 	}
-	
+
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
 		try {
@@ -56,8 +54,64 @@ public class UserRepository {
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
-		} 
-		
+		}
+
 		return conn;
-	}	
+	}
+
+	public UserVo findByEmailAndPassword(UserVo vo) {
+		UserVo result = null;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+
+			String sql = 
+			"select no, name" + 
+			"	from user" + 
+			" where email=?" + 
+			"	and password=?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, vo.getEmail());
+			pstmt.setString(2, vo.getPassword());
+			
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+
+				result = new UserVo();
+				vo.setNo(no);
+				vo.setName(name);
+
+				result = new UserVo();
+				vo.setNo(no);
+				vo.setName(name);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
 }
--- mysite02/src/main/webapp/WEB-INF/views/user/loginform.jsp
+++ mysite02/src/main/webapp/WEB-INF/views/user/loginform.jsp
@@ -14,12 +14,18 @@
 				<form id="login-form" name="loginform" method="post" action="<%=request.getContextPath() %>/user">
 					<input type = "hidden" name="a" value="login" />
 					<label class="block-label" for="email">이메일</label>
-					<input id="email" name="email" type="text" value="">
+					<input id="email" name="email" type="text" value="<%=(request.getAttribute("email") == null ? "" : request.getAttribute("email")) %>">
 					<label class="block-label" >패스워드</label>
 					<input name="password" type="password" value="">
+					<%
+					if ("fail".equals(request.getAttribute("result"))) {
+					%>
 					<p>
 						로그인이 실패 했습니다.
 					</p>
+					<%
+						}
+					%>
 					<input type="submit" value="로그인">
 				</form>
 			</div>
