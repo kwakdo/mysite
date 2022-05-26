4f4f395ae7b23b804ff6e54da72790a571c0d2f1
kwakdo <vv05044@gmail.com>
2022-05-26 16:21:12 +0900

    really done

--- mysite02/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite02/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -167,6 +167,16 @@ public class UserRepository {
 		try {
 			conn = getConnection();
 
+			
+			if("".equals(vo.getPassword())) {
+				String sql = " update user" + "	set name=?, gender=?" + "	where no = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getGender());
+			pstmt.setLong(3, vo.getNo());
+			} else {
 			String sql = " update user" + "	set name=?, password=?, gender=?" + "	where no = ?";
 			pstmt = conn.prepareStatement(sql);
 
@@ -174,7 +184,7 @@ public class UserRepository {
 			pstmt.setString(2, vo.getPassword());
 			pstmt.setString(3, vo.getGender());
 			pstmt.setLong(4, vo.getNo());
-
+			}
 			int count = pstmt.executeUpdate();
 			result = count == 1;
 
--- sql/user.sql
+++ sql/user.sql
@@ -5,3 +5,10 @@ desc user;
 select * from user;
 
 select * from guestbook;
+
+select no, name from user where email='vv05044@gmail.com' and password='1234';
+
+-- findByNo
+select no, name, email, gender 
+from user 
+where no='1' and name='dooly';
