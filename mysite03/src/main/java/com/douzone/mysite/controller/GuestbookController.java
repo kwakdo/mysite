0cf923d342123546e0317d1f6b7d64c79929e3c6
kwakdo <vv05044@gmail.com>
2022-06-07 16:39:59 +0900

    fix

--- mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
@@ -31,22 +31,19 @@ public class GuestbookController {
 	@RequestMapping(value="/insert", method=RequestMethod.POST)
 	public String add(GuestbookVo vo) {
 		guestbookService.addMessage(vo);
-		System.out.println(vo);
 		return "redirect:/guestbook";
 	}
 	
 	@RequestMapping(value="/delete/{no}")
 	public String delete(@PathVariable("no") Long no, Model model)    {
 		model.addAttribute("no", no);
-		System.out.println("no: " + no);
 		return "guestbook/delete";
 	}
 	
 	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST)
 	public String delete(@PathVariable("no") Long no, @RequestParam(value="password", required=true, defaultValue="") String password) {
 		guestbookService.deleteMessage(no, password);
-		System.out.println("no: " + no +  " passowrd: " + password);
-		return "redirect:/";
+		return "redirect:/guestbook";
 	}
 	
 	
