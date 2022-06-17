5b6741385b5745aaf1d0c5e2e93c59c0e6ebafed
kwakdo <vv05044@gmail.com>
2022-06-17 12:07:43 +0900

    Web Application Initializer

--- mysite05/src/main/java/com/douzone/mysite/initializer/MysiteWebApplicationInitializer.java
+++ mysite05/src/main/java/com/douzone/mysite/initializer/MysiteWebApplicationInitializer.java
@@ -1,22 +1,45 @@
 package com.douzone.mysite.initializer;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.douzone.mysite.config.app.AppConfig;
+import com.douzone.mysite.config.web.WebConfig;
 
 public class MysiteWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return null;
+		return new Class<?>[] {AppConfig.class};
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return null;
+		return new Class<?>[] {WebConfig.class};
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		return null;
+		return new String[] {"/"};
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {new CharacterEncodingFilter("UTF-8")};
+	}
+	
+	@Override
+	protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+		DispatcherServlet servlet = (DispatcherServlet)super.createDispatcherServlet(servletAppContext);
+		
+		servlet.setThrowExceptionIfNoHandlerFound(true);
+		
+		return servlet;
 	}
 
 }
